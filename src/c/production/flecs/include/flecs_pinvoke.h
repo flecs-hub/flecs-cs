94d32c62a45341b8c14802a6c16261bd01b4619b
Lucas Girouard-Stranks <lustranks@gmail.com>
2022-07-25 09:16:22 -0400

    Fix header include

--- src/c/production/flecs/include/flecs_pinvoke.h
+++ src/c/production/flecs/include/flecs_pinvoke.h
@@ -2,16 +2,40 @@
 #include "flecs.h"
 
 // Roles
-PINVOKE_API ecs_id_t pinvoke_ECS_PAIR();
-PINVOKE_API ecs_id_t pinvoke_ECS_OVERRIDE();
+PINVOKE_API ecs_id_t pinvoke_ECS_PAIR()
+{
+    return ECS_PAIR;
+}
+
+PINVOKE_API ecs_id_t pinvoke_ECS_OVERRIDE()
+{
+    return ECS_OVERRIDE;
+}
 
 // Relationships
-PINVOKE_API ecs_entity_t pinvoke_EcsIsA();
-PINVOKE_API ecs_entity_t pinvoke_EcsDependsOn();
-PINVOKE_API ecs_entity_t pinvoke_EcsChildOf();
+PINVOKE_API ecs_entity_t pinvoke_EcsIsA()
+{
+    return EcsIsA;
+}
+
+PINVOKE_API ecs_entity_t pinvoke_EcsDependsOn()
+{
+    return EcsDependsOn;
+}
+
+PINVOKE_API ecs_entity_t pinvoke_EcsChildOf()
+{
+    return EcsChildOf;
+}
 
 // Entity tags
-PINVOKE_API ecs_entity_t pinvoke_EcsPrefab();
+PINVOKE_API ecs_entity_t pinvoke_EcsPrefab()
+{
+    return EcsPrefab;
+}
 
 // System tags
-PINVOKE_API ecs_entity_t pinvoke_EcsOnUpdate();
+PINVOKE_API ecs_entity_t pinvoke_EcsOnUpdate()
+{
+    return EcsOnUpdate;
+}
