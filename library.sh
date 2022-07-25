39d913965f6bf643307d70b5e737dc3987ec9d8c
Lucas Girouard-Stranks <lustranks@gmail.com>
2022-07-25 09:00:19 -0400

    Update native library build script

--- library.sh
+++ library.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+if [[ -z $SCRIPTS_DIRECTORY ]]; then
+    SCRIPTS_DIRECTORY="$DIRECTORY/ext/scripts"
+    git clone "https://github.com/bottlenoselabs/scripts" "$SCRIPTS_DIRECTORY" 2> /dev/null 1> /dev/null || git -C "$SCRIPTS_DIRECTORY" pull 1> /dev/null
+fi
 
-$DIR/ext/scripts/c/library/main.sh \
-    $DIR/src/c/production/flecs \
-    $DIR/lib \
+$SCRIPTS_DIRECTORY/c/library/main.sh \
+    $DIRECTORY/src/c/production/flecs \
+    $DIRECTORY/build \
+    $DIRECTORY/lib \
     "flecs" \
     "flecs" \
     "" \
