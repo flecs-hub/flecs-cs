035141349219d2b222d7c8cbca6c280a10cc098e
BlackPhoenix134 <46650884+BlackPhoenix134@users.noreply.github.com>
2022-08-10 15:52:19 +0200

    Overridable pairs (#47)
    * added overridable pairs
    
     and example

--- src/cs/examples/entities/Flecs.Examples.Entities.Prefab/Program.cs
+++ src/cs/examples/entities/Flecs.Examples.Entities.Prefab/Program.cs
@@ -37,6 +37,12 @@ public struct HasFtl : ITag
 {
 }
 
+public struct Amount : IComponent
+{
+    public int Max;
+    public int Current;
+}
+
 internal static class Program
 {
     private static int Main(string[] args)
@@ -48,13 +54,20 @@ internal static class Program
         world.RegisterComponent<Defense>();
         world.RegisterComponent<FreightCapacity>();
         world.RegisterComponent<ImpulseSpeed>();
+        world.RegisterComponent<Amount>();
         world.RegisterTag<HasFtl>();
+
+        var hull = world.CreateEntity("Hull");
+        var shield = world.CreateEntity("Shield");
 
         // Create a prefab hierarchy. Prefabs are entities that by default are
         // ignored by queries.
         var spaceShip = world.CreatePrefab("SpaceShip");
         spaceShip.SetComponent(new ImpulseSpeed { Value = 50 });
         spaceShip.SetComponent(new Defense { Value = 50 });
+        //setting as overridable, every ship instance can be damaged individually
+        spaceShip.SetPairSecondCompOverride(hull, new Amount() { Max = 200, Current = 200});
+        spaceShip.SetPairSecondCompOverride(shield, new Amount() { Max = 200, Current = 200 });
 
         // By default components in an inheritance hierarchy are shared between
         // entities. The override function ensures that instances have a private
@@ -82,27 +95,27 @@ internal static class Program
         // Create an entity from a prefab.
         // The instance will have a private copy of the Position component, because
         // of the override in the spaceship entity. All other components are shared.
-        var instance = world.CreateEntity("my_mammoth_freighter");
-        instance.IsA(mammothFreighter);
+        var freighter1 = world.CreateEntity("my_mammoth_freighter1");
+        freighter1.IsA(mammothFreighter);
+
+        var freighter2 = world.CreateEntity("my_mammoth_freighter2");
+        freighter2.IsA(mammothFreighter);
 
         // Inspect the type of the entity. This outputs:
         //    Position,(Identifier,Name),(IsA,MammothFreighter)
-        var instanceTypeString = instance.Type().String();
+        var instanceTypeString = freighter1.Type().String();
         Console.WriteLine("Instance type: " + instanceTypeString);
         
         // Even though the instance doesn't have a private copy of ImpulseSpeed, we
         // can still get it using the regular API (outputs 50)
-        var impulseSpeed = instance.GetComponent<ImpulseSpeed>();
+        var impulseSpeed = freighter1.GetComponent<ImpulseSpeed>();
         Console.WriteLine("Impulse speed: " + impulseSpeed.Value);
-        
-        // Prefab components can be iterated like regular components:
-        // var queryDescriptor = new QueryDescriptor();
-        // var query = world.CreateQuery(ref queryDescriptor);
-        //     .filter.terms = {
-        //         // To select components from a prefab, use SuperSet
-        //         { .id = ecs_id(ImpulseSpeed), .subj.set.mask = EcsSuperSet }, 
-        //         { .id = ecs_id(Position) }
-        // });
+
+        ref var freighter1Hull = ref freighter1.GetPairSecondComp<Amount>(hull);
+        freighter1Hull.Current -= 100;
+
+        Console.WriteLine($"Freigher 1 hull: {freighter1.GetPairSecondComp<Amount>(hull).Current}");
+        Console.WriteLine($"Freigher 2 hull: {freighter2.GetPairSecondComp<Amount>(hull).Current}");
 
         return world.Fini();
     }
