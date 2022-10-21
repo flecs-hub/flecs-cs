ff51bd221d370334e786b5838f5df3968d67df97
Lucas Girouard-Stranks <lithiumtoast@users.noreply.github.com>
2022-10-21 11:52:22 -0400

    Use generic path for c2cs

--- bindgen/read_c_code.sh
+++ bindgen/read_c_code.sh
@@ -2,4 +2,4 @@
 DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 dotnet build "$DIRECTORY/../src/cs/production/Flecs.Bindgen/Flecs.Bindgen.csproj" -p:OutputPath="$DIRECTORY/plugins/Flecs.Bindgen"
-/Users/lstranks/.dotnet/tools/c2cs c
+c2cs c
