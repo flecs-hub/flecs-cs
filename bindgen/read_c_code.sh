b19c99d3f5e38387f67dfef2c389185fd3b3718a
Lucas Girouard-Stranks <lithiumtoast@users.noreply.github.com>
2023-03-28 22:03:55 -0400

    Use CAstFfi

--- bindgen/read_c_code.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-
-dotnet build "$DIRECTORY/../src/cs/production/Flecs.Bindgen/Flecs.Bindgen.csproj" -p:OutputPath="$DIRECTORY/plugins/Flecs.Bindgen"
-c2cs c
