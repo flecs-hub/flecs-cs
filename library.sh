16d52c461272abb2bd81712c5ec61a8f6a4680d5
Lucas Girouard-Stranks <lithiumtoast@users.noreply.github.com>
2023-05-03 18:26:45 -0400

    Use C2CS v5.1 `library` command

--- library.sh
+++ library.sh
@@ -1,18 +1,7 @@
 #!/bin/bash
 DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-if [[ -z $SCRIPTS_DIRECTORY ]]; then
-    SCRIPTS_DIRECTORY="$DIRECTORY/ext/scripts"
-    git clone "https://github.com/bottlenoselabs/scripts" "$SCRIPTS_DIRECTORY" 2> /dev/null 1> /dev/null || git -C "$SCRIPTS_DIRECTORY" pull 1> /dev/null
-fi
 
-$SCRIPTS_DIRECTORY/c/library/main.sh \
-    $DIRECTORY/src/c/production/flecs \
-    $DIRECTORY/build \
-    $DIRECTORY/lib \
-    "flecs" \
-    "flecs" \
-    "" \
-    "" \
+c2cs library --config "./bindgen/config-library.json"
 	
 if [[ -z "$1" ]]; then
     read
