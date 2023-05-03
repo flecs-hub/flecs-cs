e623d07cc1d2441582dbb765b76c99dbb4afc832
Lucas Girouard-Stranks <lithiumtoast@users.noreply.github.com>
2023-05-03 19:02:27 -0400

    Upgrade to C2CS v5.1

--- library.sh
+++ library.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-c2cs library --config "./bindgen/config-library.json"
+if ! [[ -x "$(command -v c2cs)" ]]; then
+  echo "Error: 'c2cs' is not installed. The C2CS tool is used to generate a C shared library for the purposes of P/Invoke with C#. Please visit https://github.com/bottlenoselabs/C2CS for instructions to install the C2CS tool." >&2
+  exit 1
+fi
+
+c2cs library --config "./bindgen/config-build-c-library.json"
 	
 if [[ -z "$1" ]]; then
     read
